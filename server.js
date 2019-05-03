412b59cdcea1efb776dc66c0c396ff5901a18d43
Nils Deusch <ndeusch@veka.com>
2019-05-03 12:38:06 +0200

    Server Log on filesystem commented

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}:${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {
-      console.log('Unable to append to server.log.');
-    }
-  })
+  // fs.appendFile('server.log', log + '\n', (err) => {
+  //   if (err) {
+  //     console.log('Unable to append to server.log.');
+  //   }
+  // })
   next();
 });
 
